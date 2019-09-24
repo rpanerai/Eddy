c14d869b2bbec4cdfdb53bf409678748ce44b62c
Rodolfo Panerai <rodolfo.panerai@gmail.com>
2019-09-24 14:05:54 +0200

    Add widget for author-count capping

--- eddy/gui/searchfilter.py
+++ eddy/gui/searchfilter.py
@@ -1,6 +1,39 @@
 from PySide2.QtCore import Signal
 from PySide2.QtGui import QIcon
-from PySide2.QtWidgets import QWidget, QHBoxLayout, QPushButton, QLineEdit
+from PySide2.QtWidgets import QWidget, QHBoxLayout, QPushButton, QLineEdit, QCheckBox, QSpinBox
+
+
+class ACCapWidget(QWidget):
+    def __init__(self, parent=None):
+        super(ACCapWidget, self).__init__(parent)
+
+        self._check = QCheckBox()
+        self._check.setText("AC ≤")
+        # self._check.setStyleSheet("QCheckBox:checked{color: black;} QCheckBox:unchecked{color: grey;}")
+
+        self._spin = QSpinBox()
+        self._spin.setRange(1, 99)
+        self._spin.setValue(10)
+        self._check.toggled.connect(self._spin.setEnabled)
+
+        self.SetEnabled(False)
+
+        layout = QHBoxLayout()
+        layout.addWidget(self._check)
+        layout.addWidget(self._spin)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        self.setLayout(layout)
+
+    def SetEnabled(self, bool_):
+        self._check.setChecked(bool_)
+        self._spin.setEnabled(bool_)
+
+    def IsChecked(self):
+        return self._check.isChecked()
+
+    def Value(self):
+        return self._spin.value()
 
 
 class SearchBar(QWidget):
@@ -21,9 +54,12 @@ class SearchBar(QWidget):
         self._kill_button.setEnabled(False)
         self._kill_button.clicked.connect(self._HandleStopPressed)
 
+        self._ac_cap = ACCapWidget()
+
         layout = QHBoxLayout()
         layout.addWidget(self._kill_button)
         layout.addWidget(self._query_edit)
+        layout.addWidget(self._ac_cap)
         layout.setContentsMargins(0, 0, 0, 0)
 
         self.setLayout(layout)
@@ -36,6 +72,8 @@ class SearchBar(QWidget):
 
     def _HandleReturnPressed(self):
         search_string = " ".join(self._query_edit.text().split())
+        if self._ac_cap.IsChecked():
+            search_string = search_string + " and ac <= " + str(self._ac_cap.Value())
         self.SearchRequested.emit(search_string)
 
     def _HandleStopPressed(self):
