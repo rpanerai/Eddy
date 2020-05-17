c7eedb8f8e441f03be421a518198e55325199f42
Rodolfo Panerai <rodolfo.panerai@gmail.com>
2020-05-17 19:49:37 +0200

    Add date field to ItemWidget

--- eddy/gui/item.py
+++ eddy/gui/item.py
@@ -1,4 +1,5 @@
 from functools import partial
+from datetime import datetime
 
 from PySide2.QtCore import Signal
 from PySide2.QtGui import QFont, QIcon
@@ -18,6 +19,7 @@ class ItemWidget(QWidget):
         "title",
         "authors",
         "abstract",
+        "date",
         "journal",
         "texkey",
         "inspire_id",
@@ -58,6 +60,7 @@ class ItemWidget(QWidget):
         for (k, v) in ItemWidget._FORMAT_TYPE.items():
             self._type.addItem(v, k)
 
+        self._date = QLineEdit()
         self._journal = QLineEdit()
         self._texkey = QLineEdit()
         # copy_texkey = self._texkey.addAction(
@@ -102,6 +105,7 @@ class ItemWidget(QWidget):
         form_layout.addRow("Title", self._title)
         form_layout.addRow("Authors", self._authors)
         form_layout.addRow("Abstract", self._abstract)
+        form_layout.addRow("Date", self._date)
         form_layout.addRow("Journal", self._journal)
         form_layout.addRow("BibTeX", self._texkey)
         form_layout.addRow("INSPIRE", self._inspire_id)
@@ -129,10 +133,14 @@ class ItemWidget(QWidget):
         self._save.setEnabled(False)
 
         self._type.currentIndexChanged.connect(self._EnableRefreshSave)
+        self._date.textEdited.connect(self._EnableRefreshSave)
         self._journal.textEdited.connect(self._EnableRefreshSave)
         self._texkey.textEdited.connect(self._EnableRefreshSave)
         self._inspire_id.textEdited.connect(self._EnableRefreshSave)
         self._arxiv_id.textEdited.connect(self._EnableRefreshSave)
+        # Note that the textChanged() signal is triggered even when the text is modified
+        # programmatically. This is not problem since we disable the buttons immediately after the
+        # QTextEdit widgets are updated in DisplayItem().
         self._title.textChanged.connect(self._EnableRefreshSave)
         self._authors.textChanged.connect(self._EnableRefreshSave)
         self._abstract.textChanged.connect(self._EnableRefreshSave)
@@ -158,6 +166,14 @@ class ItemWidget(QWidget):
 
         abstract = self._abstract.toPlainText().strip()
         data["abstract"] = abstract if abstract != "" else None
+
+        date = self._date.text().strip()
+        if date == "":
+            data["date"] = None
+        else:
+            date = ItemWidget._ParseDate(date)
+            if date is not None:
+                data["date"] = date
 
         journal = self._journal.text().strip()
         data["journal"] = journal if journal != "" else None
@@ -214,6 +230,7 @@ class ItemWidget(QWidget):
         self._title.setPlainText(record["title"])
         self._authors.setPlainText(record["authors"])
         self._abstract.setPlainText(record["abstract"])
+        self._date.setText(record["date"])
         self._journal.setText(record["journal"])
         self._texkey.setText(record["texkey"])
         self._inspire_id.setText(record["inspire_id"])
@@ -225,6 +242,26 @@ class ItemWidget(QWidget):
 
         self._reload.setEnabled(False)
         self._save.setEnabled(False)
+
+    @staticmethod
+    def _ParseDate(date_string):
+        n_dashes = date_string.count("-")
+        if n_dashes == 0:
+            try:
+                return datetime.strptime(date_string, "%Y").strftime("%Y")
+            except:
+                return None
+        if n_dashes == 1:
+            try:
+                return datetime.strptime(date_string, "%Y-%m").strftime("%Y-%m")
+            except:
+                return None
+        if n_dashes == 2:
+            try:
+                return datetime.strptime(date_string, "%Y-%m-%d").strftime("%Y-%m-%d")
+            except:
+                return None
+        return None
 
 
 class AdaptiveTextEdit(QTextEdit):
