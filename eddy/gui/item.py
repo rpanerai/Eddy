8371f540f260d3793c820b01edb7224fc2871ad3
Rodolfo Panerai <rodolfo.panerai@gmail.com>
2020-09-30 18:06:33 +0200

    Refactor ItemWidget

--- eddy/gui/item.py
+++ eddy/gui/item.py
@@ -79,47 +79,67 @@ class ItemWidget(QWidget):
         # self._scroll.setBackgroundRole(QPalette.Base)
         self._scroll.setWidgetResizable(True)
 
-        self._type = QComboBox()
+        self._date = LineEdit()
+        min_height = self._date.sizeHint().height()
+        self._type = ComboBox()
+        self._title = MultiLineTextEdit(min_height)
+        self._authors = LineSplitTextEdit(min_height)
+        self._editors = LineSplitTextEdit(min_height)
+        self._abstract = MultiLineTextEdit(min_height)
+        self._publication = LineEdit()
+        self._volume = LineEdit()
+        self._issue = LineEdit()
+        self._pages = LineEdit()
+        self._year = LineEdit()
+        self._edition = LineEdit()
+        self._series = LineEdit()
+        self._publisher = LineEdit()
+        self._isbns = LineSplitTextEdit(min_height)
+        self._institution = LineEdit()
+        self._degree = LineEdit()
+        self._texkey = LineEdit()
+        self._inspire_id = LineEdit()
+        self._arxiv_id = LineEdit()
+        self._dois = LineSplitTextEdit(min_height)
+        self._urls = LineSplitTextEdit(min_height)
+        self._files = LineSplitTextEdit(min_height)
+
         for (k, v) in ItemWidget._FORMAT_TYPE.items():
             self._type.addItem(v, k)
-
         # self._type.currentIndexChanged[int].connect(self._RefreshTypeFields)
 
-        self._details_frame = QGroupBox()
-        self._details_layout = QFormLayout()
-
-        self._date = QLineEdit()
-        self._publication = QLineEdit()
-        self._volume = QLineEdit()
-        self._issue = QLineEdit()
-        self._pages = QLineEdit()
-        self._year = QLineEdit()
-        self._edition = QLineEdit()
-        self._series = QLineEdit()
-        self._publisher = QLineEdit()
-        self._institution = QLineEdit()
-        self._degree = QLineEdit()
-        self._texkey = QLineEdit()
-        # copy_texkey = self._texkey.addAction(
-        #     QIcon.fromTheme("edit-copy"), QLineEdit.TrailingPosition
-        # )
-        self._inspire_id = QLineEdit()
-        self._arxiv_id = QLineEdit()
-
-        min_height = self._date.sizeHint().height()
-
-        self._title = AdaptiveTextEdit(min_height)
         bold_font = self._title.document().defaultFont()
         bold_font.setBold(True)
         self._title.document().setDefaultFont(bold_font)
 
-        self._authors = AdaptiveTextEdit(min_height)
-        self._editors = AdaptiveTextEdit(min_height)
-        self._abstract = AdaptiveTextEdit(min_height)
-        self._isbns = AdaptiveTextEdit(min_height)
-        self._dois = AdaptiveTextEdit(min_height)
-        self._urls = AdaptiveTextEdit(min_height)
-        self._files = AdaptiveTextEdit(min_height)
+        self._fields = {
+            "type": self._type,
+            "title": self._title,
+            "authors": self._authors,
+            "editors": self._editors,
+            "abstract": self._abstract,
+            "date": self._date,
+            "publication": self._publication,
+            "volume": self._volume,
+            "issue": self._issue,
+            "pages": self._pages,
+            "year": self._year,
+            "edition": self._edition,
+            "series": self._series,
+            "publisher": self._publisher,
+            "isbns": self._isbns,
+            "institution": self._institution,
+            "degree": self._degree,
+            "texkey": self._texkey,
+            "inspire_id": self._inspire_id,
+            "arxiv_id": self._arxiv_id,
+            "dois": self._dois,
+            "urls": self._urls,
+            "files": self._files
+        }
+
+        self._details_frame = QGroupBox()
+        self._details_layout = QFormLayout()
 
         self._scroll_widget = QWidget()
         self._SetupScrollUI()
@@ -200,55 +220,22 @@ class ItemWidget(QWidget):
         self._save.setEnabled(False)
         self._file_add.setEnabled(False)
 
-        self._type.currentIndexChanged.connect(self._EnableRefreshSave)
-        self._date.textEdited.connect(self._EnableRefreshSave)
-        self._publication.textEdited.connect(self._EnableRefreshSave)
-        self._volume.textEdited.connect(self._EnableRefreshSave)
-        self._issue.textEdited.connect(self._EnableRefreshSave)
-        self._pages.textEdited.connect(self._EnableRefreshSave)
-        self._year.textEdited.connect(self._EnableRefreshSave)
-        self._edition.textEdited.connect(self._EnableRefreshSave)
-        self._series.textEdited.connect(self._EnableRefreshSave)
-        self._publisher.textEdited.connect(self._EnableRefreshSave)
-        self._institution.textEdited.connect(self._EnableRefreshSave)
-        self._degree.textEdited.connect(self._EnableRefreshSave)
-        self._texkey.textEdited.connect(self._EnableRefreshSave)
-        self._inspire_id.textEdited.connect(self._EnableRefreshSave)
-        self._arxiv_id.textEdited.connect(self._EnableRefreshSave)
-        # Note that the textChanged() signal is triggered even when the text is modified
-        # programmatically. We use self._updating to inhibit the slot.
-        self._title.textChanged.connect(self._EnableRefreshSave)
-        self._authors.textChanged.connect(self._EnableRefreshSave)
-        self._editors.textChanged.connect(self._EnableRefreshSave)
-        self._abstract.textChanged.connect(self._EnableRefreshSave)
-        self._isbns.textChanged.connect(self._EnableRefreshSave)
-        self._dois.textChanged.connect(self._EnableRefreshSave)
-        self._urls.textChanged.connect(self._EnableRefreshSave)
-        self._files.textChanged.connect(self._EnableRefreshSave)
+        for w in self._fields.values():
+            w.Edited.connect(self._EnableRefreshSave)
 
     # def _RefreshTypeFields(self, index):
     #     pass
 
     def _EnableRefreshSave(self):
-        if self._updating:
-            return
-
         self._reload.setEnabled(True)
         self._save.setEnabled(True)
 
     def _Save(self):
         data = {}
+        for (k, w) in self._fields.items():
+            data[k] = w.Read()
 
-        data["type"] = self._type.currentData()
-
-        title = self._title.toPlainText().strip()
-        data["title"] = title if title != "" else None
-
-        authors_raw = [
-            a for a in
-            [" ".join(a.split()) for a in self._authors.toPlainText().splitlines()]
-            if a != ""
-        ]
+        authors_raw = [" ".join(a.split()) for a in data["authors"]]
         authors = []
         authors_bais = []
         for a in authors_raw:
@@ -258,11 +245,7 @@ class ItemWidget(QWidget):
         data["authors"] = authors
         data["authors_bais"] = authors_bais
 
-        editors_raw = [
-            e for e in
-            [" ".join(e.split()) for e in self._editors.toPlainText().splitlines()]
-            if e != ""
-        ]
+        editors_raw = [" ".join(e.split()) for e in data["editors"]]
         editors = []
         editors_bais = []
         for e in editors_raw:
@@ -272,91 +255,23 @@ class ItemWidget(QWidget):
         data["editors"] = editors
         data["editors_bais"] = editors_bais
 
-        abstract = self._abstract.toPlainText().strip()
-        data["abstract"] = abstract if abstract != "" else None
-
-        date = self._date.text().strip()
-        if date == "":
-            data["date"] = None
-        else:
-            date = ItemWidget._ParseDate(date)
-            if date is not None:
-                data["date"] = date
-
-        publication = self._publication.text().strip()
-        data["publication"] = publication if publication != "" else None
-
-        volume = self._volume.text().strip()
-        data["volume"] = volume if volume != "" else None
-
-        issue = self._issue.text().strip()
-        data["issue"] = issue if issue != "" else None
-
-        pages = self._pages.text().strip()
-        data["pages"] = pages if pages != "" else None
-
-        year = self._year.text().strip()
-        if year == "":
-            data["year"] = None
-        else:
+        if data["date"] is not None:
             try:
-                data["year"] = int(year)
+                data["date"] = ItemWidget._ParseDate(data["date"])
             except:
-                pass
+                del data["date"]
 
-        edition = self._edition.text().strip()
-        data["edition"] = edition if edition != "" else None
-
-        series = self._series.text().strip()
-        data["series"] = series if series != "" else None
-
-        publisher = self._publisher.text().strip()
-        data["publisher"] = publisher if publisher != "" else None
-
-        data["isbns"] = [
-            i for i in
-            [" ".join(i.split()) for i in self._isbns.toPlainText().splitlines()]
-            if i != ""
-        ]
-
-        institution = self._institution.text().strip()
-        data["institution"] = institution if institution != "" else None
-
-        degree = self._degree.text().strip()
-        data["degree"] = degree if degree != "" else None
-
-        texkey = self._texkey.text().strip()
-        data["texkey"] = texkey if texkey != "" else None
-
-        inspire_id = self._inspire_id.text().strip()
-        if inspire_id == "":
-            data["inspire_id"] = None
-        else:
+        if data["year"] is not None:
             try:
-                data["inspire_id"] = int(inspire_id)
+                data["year"] = datetime.strptime(data["year"], "%Y").strftime("%Y")
             except:
-                pass
+                del data["year"]
 
-        arxiv_id = self._arxiv_id.text().strip()
-        data["arxiv_id"] = arxiv_id if arxiv_id != "" else None
-
-        data["dois"] = [
-            d for d in
-            [d.strip() for d in self._dois.toPlainText().splitlines()]
-            if d != ""
-        ]
-
-        data["urls"] = [
-            u for u in
-            [u.strip() for u in self._urls.toPlainText().splitlines()]
-            if u != ""
-        ]
-
-        data["files"] = [
-            f for f in
-            [f.strip() for f in self._files.toPlainText().splitlines()]
-            if f != ""
-        ]
+        if data["inspire_id"] is not None:
+            try:
+                data["inspire_id"] = int(data["inspire_id"])
+            except:
+                del data["inspire_id"]
 
         self._table.EditRow(self._id, data)
 
@@ -385,10 +300,14 @@ class ItemWidget(QWidget):
     def _AppendFile(self, file_name):
         data = self._table.GetRow(self._id, ("files",))
         data["files"].append(file_name)
+
+        # Here self._updating is used to prevent calls to DisplayItem()
+        # triggered by an Updated() signal emitted by the database.
         self._updating = True
         self._table.EditRow(self._id, data)
-        self._files.setPlainText(ItemWidget._FORMAT_FUNCTIONS["files"](data["files"]))
         self._updating = False
+
+        self._files.Write(ItemWidget._FORMAT_FUNCTIONS["files"](data["files"]))
 
     def SetLocalSource(self, source):
         if self._source == source:
@@ -452,31 +371,8 @@ class ItemWidget(QWidget):
         for (k, v) in ItemWidget._FORMAT_FUNCTIONS.items():
             record[k] = v(record[k])
 
-        self._updating = True
-        self._type.setCurrentText(record["type"])
-        self._title.setPlainText(record["title"])
-        self._authors.setPlainText(record["authors"])
-        self._editors.setPlainText(record["editors"])
-        self._abstract.setPlainText(record["abstract"])
-        self._date.setText(record["date"])
-        self._publication.setText(record["publication"])
-        self._volume.setText(record["volume"])
-        self._issue.setText(record["issue"])
-        self._pages.setText(record["pages"])
-        self._year.setText(record["year"])
-        self._edition.setText(record["edition"])
-        self._series.setText(record["series"])
-        self._publisher.setText(record["publisher"])
-        self._isbns.setPlainText(record["isbns"])
-        self._institution.setText(record["institution"])
-        self._degree.setText(record["degree"])
-        self._texkey.setText(record["texkey"])
-        self._inspire_id.setText(record["inspire_id"])
-        self._arxiv_id.setText(record["arxiv_id"])
-        self._dois.setPlainText(record["dois"])
-        self._urls.setPlainText(record["urls"])
-        self._files.setPlainText(record["files"])
-        self._updating = False
+        for (k, w) in self._fields.items():
+            w.Write(record[k])
 
         self._scroll.verticalScrollBar().show()
         self._scroll_widget.show()
@@ -505,19 +401,54 @@ class ItemWidget(QWidget):
     @staticmethod
     def _ParseDate(date_string):
         n_dashes = date_string.count("-")
-        try:
-            if n_dashes == 0:
-                return datetime.strptime(date_string, "%Y").strftime("%Y")
-            if n_dashes == 1:
-                return datetime.strptime(date_string, "%Y-%m").strftime("%Y-%m")
-            if n_dashes == 2:
-                return datetime.strptime(date_string, "%Y-%m-%d").strftime("%Y-%m-%d")
-        except:
-            pass
-        return None
+        if n_dashes == 2:
+            return datetime.strptime(date_string, "%Y-%m-%d").strftime("%Y-%m-%d")
+        if n_dashes == 1:
+            return datetime.strptime(date_string, "%Y-%m").strftime("%Y-%m")
+        return datetime.strptime(date_string, "%Y").strftime("%Y")
+
+
+class ComboBox(QComboBox):
+    Edited = Signal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self._editing_call = False
+        self.currentIndexChanged.connect(self._HandleCurrentIndexChanged)
+
+    def _HandleCurrentIndexChanged(self):
+        if not self._editing_call:
+            self.Edited.emit()
+
+    def Write(self, text):
+        self._editing_call = True
+        super().setCurrentText(text)
+        self._editing_call = False
+
+    def Read(self):
+        return self.currentData()
+
+
+class LineEdit(QLineEdit):
+    Edited = Signal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.textEdited.connect(self.Edited)
+
+    def Write(self, text):
+        super().setText(text)
+
+    def Read(self):
+        text = self.text().strip()
+        return text if text != "" else None
 
 
 class AdaptiveTextEdit(QTextEdit):
+    Edited = Signal()
+
     def __init__(self, min_height, parent=None):
         # One might want to add a mechanism for setting a max_height.
         super().__init__(parent)
@@ -526,6 +457,9 @@ class AdaptiveTextEdit(QTextEdit):
         self.setTabChangesFocus(True)
         self.document().documentLayout().documentSizeChanged.connect(self.Resize)
 
+        self._editing_call = False
+        self.textChanged.connect(self._HandleTextChanged)
+
     def focusOutEvent(self, event):
         cursor = self.textCursor()
         cursor.clearSelection()
@@ -533,9 +467,30 @@ class AdaptiveTextEdit(QTextEdit):
 
         super().focusOutEvent(event)
 
+    def Write(self, text):
+        self._editing_call = True
+        super().setPlainText(text)
+        self._editing_call = False
+
+    def _HandleTextChanged(self):
+        if not self._editing_call:
+            self.Edited.emit()
+
     def Resize(self, new_size):
         margins = self.contentsMargins()
         document_size = self.document().documentLayout().documentSize()
         height = document_size.height() + margins.top() + margins.bottom()
         # self.setMinimumHeight(max(height, self.min_height))
         self.setFixedHeight(max(height, self.min_height))
+
+
+class MultiLineTextEdit(AdaptiveTextEdit):
+    def Read(self):
+        text = self.toPlainText().strip()
+        return text if text != "" else None
+
+
+class LineSplitTextEdit(AdaptiveTextEdit):
+    def Read(self):
+        lines = [l.strip() for l in self.toPlainText().splitlines()]
+        return [l for l in lines if l != ""]
