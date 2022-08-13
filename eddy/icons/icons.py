96abbee7ea3773f25576ef1204000dfeadff1638
Rodolfo Panerai <rodolfo.panerai@gmail.com>
2022-08-13 21:41:31 +0200

    Switch from os.path to pathlib in icons.py

--- eddy/icons/icons.py
+++ eddy/icons/icons.py
@@ -1,39 +1,39 @@
-import os
+from pathlib import Path
 
 from PySide2.QtCore import QFileInfo
 from PySide2.QtWidgets import QFileIconProvider
 
 
-ICONS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+ICONS_FOLDER = Path(__file__).resolve().parent
 
-ARXIV = os.path.join(ICONS_FOLDER, "arxiv.ico")
-DOI = os.path.join(ICONS_FOLDER, "doi.ico")
-INSPIRE = os.path.join(ICONS_FOLDER, "inspire.ico")
+ARXIV = str(ICONS_FOLDER / "arxiv.ico")
+DOI = str(ICONS_FOLDER / "doi.ico")
+INSPIRE = str(ICONS_FOLDER / "inspire.ico")
 
-BREEZE_FOLDER = os.path.join(ICONS_FOLDER, "breeze")
+BREEZE_FOLDER = ICONS_FOLDER / "breeze"
 
-ADD = os.path.join(BREEZE_FOLDER, "add.svg")
-DATABASE = os.path.join(BREEZE_FOLDER, "database.svg")
-DELETE = os.path.join(BREEZE_FOLDER, "delete.svg")
-FILE = os.path.join(BREEZE_FOLDER, "file.svg")
-FILES = os.path.join(BREEZE_FOLDER, "files.svg")
-FILE_ADD = os.path.join(BREEZE_FOLDER, "file_add.svg")
-FILE_CHECK = os.path.join(BREEZE_FOLDER, "file_check.svg")
-FILE_DELETE = os.path.join(BREEZE_FOLDER, "file_delete.svg")
-FILTER = os.path.join(BREEZE_FOLDER, "filter.svg")
-LOCAL = os.path.join(BREEZE_FOLDER, "local.svg")
-OPEN = os.path.join(BREEZE_FOLDER, "open.svg")
-PDF = os.path.join(BREEZE_FOLDER, "pdf.svg")
-QUIT = os.path.join(BREEZE_FOLDER, "quit.svg")
-RELOAD = os.path.join(BREEZE_FOLDER, "reload.svg")
-SAVE = os.path.join(BREEZE_FOLDER, "save.svg")
-SEARCH = os.path.join(BREEZE_FOLDER, "search.svg")
-STOP = os.path.join(BREEZE_FOLDER, "stop.svg")
-TAB_CLOSE = os.path.join(BREEZE_FOLDER, "tab_close.svg")
-TAB_NEW = os.path.join(BREEZE_FOLDER, "tab_new.svg")
-TAG = os.path.join(BREEZE_FOLDER, "tag.svg")
-TAG_NEW = os.path.join(BREEZE_FOLDER, "tag_new.svg")
-WEB = os.path.join(BREEZE_FOLDER, "web.svg")
+ADD = str(BREEZE_FOLDER / "add.svg")
+DATABASE = str(BREEZE_FOLDER / "database.svg")
+DELETE = str(BREEZE_FOLDER / "delete.svg")
+FILE = str(BREEZE_FOLDER / "file.svg")
+FILES = str(BREEZE_FOLDER / "files.svg")
+FILE_ADD = str(BREEZE_FOLDER / "file_add.svg")
+FILE_CHECK = str(BREEZE_FOLDER / "file_check.svg")
+FILE_DELETE = str(BREEZE_FOLDER / "file_delete.svg")
+FILTER = str(BREEZE_FOLDER / "filter.svg")
+LOCAL = str(BREEZE_FOLDER / "local.svg")
+OPEN = str(BREEZE_FOLDER / "open.svg")
+PDF = str(BREEZE_FOLDER / "pdf.svg")
+QUIT = str(BREEZE_FOLDER / "quit.svg")
+RELOAD = str(BREEZE_FOLDER / "reload.svg")
+SAVE = str(BREEZE_FOLDER / "save.svg")
+SEARCH = str(BREEZE_FOLDER / "search.svg")
+STOP = str(BREEZE_FOLDER / "stop.svg")
+TAB_CLOSE = str(BREEZE_FOLDER / "tab_close.svg")
+TAB_NEW = str(BREEZE_FOLDER / "tab_new.svg")
+TAG = str(BREEZE_FOLDER / "tag.svg")
+TAG_NEW = str(BREEZE_FOLDER / "tag_new.svg")
+WEB = str(BREEZE_FOLDER / "web.svg")
 
 def FileIcon(file_):
     return QFileIconProvider().icon(QFileInfo(file_))
