58121bc06710c9168b68bee4e4ab41826b9ef178
Rodolfo Panerai <rodolfo.panerai@gmail.com>
2023-10-12 15:45:52 +0200

    Introduce subcommands in eddyctl.py syntax

--- eddyctl.py
+++ eddyctl.py
@@ -2,26 +2,35 @@
 
 import sys
 from pathlib import Path
-import argparse
-import pathlib
 
 ROOT_DIR = Path(__file__).resolve().parent
 sys.path.insert(0, str(ROOT_DIR))
+
+import argparse
 
 from eddy.database.database import Database
 from eddy.database.items import ItemsTable
 from eddy.database.tags import TagsTable
 
+
+def NewDatabase(file):
+    if file.exists():
+        print(f"{sys.argv[0]}: Cannot create database file ‘{file}’: File exists")
+    else:
+        database = Database(file)
+        ItemsTable(database).Clear()
+        TagsTable(database).Clear()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--new", metavar="FILE", type=pathlib.Path,
-        help="creates an Eddy database in FILE")
+    subparsers = parser.add_subparsers(title="commands", required=True, dest="command")
+
+    parser_new = subparsers.add_parser("new", help="creates an Eddy database")
+    parser_new.add_argument("FILE", type=Path, help="the file name of the new database")
+
     args = parser.parse_args()
 
-    if args.new:
-        if args.new.exists():
-            print(f"{sys.argv[0]}: Cannot create database file ‘{args.new}’: File exists")
-        else:
-            database = Database(args.new)
-            ItemsTable(database).Clear()
-            TagsTable(database).Clear()
+    match args.command:
+        case "new":
+            NewDatabase(args.FILE)
